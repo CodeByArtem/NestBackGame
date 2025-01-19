78dfd6b6eaa1af79d4b184d1b6fe9ff28239f8f1
CodeByArtem <smailxxxvizde@gmail.com>
2025-01-19 22:28:02 +0100

    post get delet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { UsersController } from './users/users.controller';
-import { UserController } from './user/user.controller';
-import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from '@user/user.module';
+import { PrismaModule } from '@prisma/prisma.module';
+
 
 @Module({
-    imports: [PrismaModule],
-    controllers: [AppController, UsersController, UserController],
-    providers: [AppService, UserService],
+    imports: [UserModule, PrismaModule],
+
 })
 export class AppModule {}
