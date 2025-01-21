ea83d0f144f3eff4659f5211b32c1ff98b793425
CodeByArtem <smailxxxvizde@gmail.com>
2025-01-21 17:57:22 +0100

    Roles

--- src/auth/guard/index.ts
+++ src/auth/guard/index.ts
@@ -1,3 +1,4 @@
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { RolesGuard } from '@auth/guard/role.guard';
 
-export const GUARDS = [JwtAuthGuard];
+export const GUARDS = [JwtAuthGuard, RolesGuard];
